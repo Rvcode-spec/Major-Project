d80eed04b60c43caa9a19aa5aae8bdbe7822fd18
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-10-08 10:23:44 +0530

    study.in Home Page

---
+++ satvik/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Index from './Satvik App/Satvik';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Index/>
+  </React.StrictMode>
+);
+
+
+
+
