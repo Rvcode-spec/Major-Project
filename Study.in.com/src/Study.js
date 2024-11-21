aa5fdcfc5e74e7aa22c79430709972f71d763c24
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-11-22 01:19:31 +0530

    Add on Login and Signup Code

---
+++ Study.in.com/src/Study.js
@@ -0,0 +1,9 @@
+function showSignup() {
+    document.querySelector("container1").display = "block";
+    document.querySelector("container2").display = "flex"; // Show Signup container
+}
+
+function showLogin() {
+    document.querySelector("container2").display = "block";
+    document.querySelector("container1").display = "flex"; // Show Login container
+}
