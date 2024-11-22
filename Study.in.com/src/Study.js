7cd2ad9ec05a7bbd14f0620302236bc09c9cc494
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-11-22 22:55:24 +0530

    Study.in add  Close-icon

--- Study.in.com/src/Study.js
+++ Study.in.com/src/Study.js
@@ -7,3 +7,9 @@ function showLogin() {
     document.querySelector("container2").display = "block";
     document.querySelector("container1").display = "flex"; // Show Login container
 }
+
+
+function goToNext() {
+    // Redirect to the desired page
+    window.location.href = "/Study.in-H/first.html";
+}
