857aa8e3685ac64a224528282c33945f130965bd
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-12-02 22:31:29 +0530

    Update the code.

--- Study.in.com/src/Study.js
+++ Study.in.com/src/Study.js
@@ -1,11 +1,25 @@
-document.getElementById("to-signup").addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent page reload
-    document.getElementById("login-form").style.display = "none";
-    document.getElementById("signup-form").style.display = "block";
+// JavaScript to handle the sign up link click
+document.getElementById("showSignup").addEventListener("click", function(event) {
+    event.preventDefault(); // Default link behavior ko rokein
+    document.getElementById("container1").style.display = "none"; // Login container ko chhupa dein
+    document.getElementById("container2").style.display = "block"; // Signup container ko dikhayein
 });
 
-document.getElementById("to-login").addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent page reload
-    document.getElementById("signup-form").style.display = "none";
-    document.getElementById("login-form").style.display = "block";
+// JavaScript to handle the log in link click
+document.getElementById("showLogin").addEventListener("click", function(event) {
+    event.preventDefault(); // Default link behavior ko rokein
+    document.getElementById("container2").style.display = "none"; // Signup container ko chhupa dein
+    document.getElementById("container1").style.display = "block"; // Login container ko dikhayein
 });
+
+// // JavaScript to handle the close icon click in Signup-Container
+// document.querySelector("#container2 .close-icon").addEventListener("click", function() {
+//     document.getElementById("container2").style.display = "none"; // Signup container ko chhupa dein
+//     document.getElementById("container1").style.display = "block"; // Login container ko dikhayein
+// });
+
+// // JavaScript to handle the close icon click in Login-Container
+// document.querySelector("#container1 .close-icon").addEventListener("click", function() {
+//     document.getElementById("container1").style.display = "none"; // Login container ko chhupa dein
+//     document.getElementById("container2").style.display = "block"; // Signup container ko dikhayein
+// });
