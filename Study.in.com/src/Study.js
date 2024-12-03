ec706c2c2316b78583773d60c79f29bb4f50e3ad
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-12-04 00:30:04 +0530

    add the js

--- Study.in.com/src/Study.js
+++ Study.in.com/src/Study.js
@@ -2,6 +2,15 @@
 const loginBox = document.getElementById('loginBox');
 const signUpBox = document.getElementById('signUpBox');
 const closeIcons = document.querySelectorAll('.close-icon');
+const loginBtn = document.querySelector('.login-Btn');
+
+loginBtn.addEventListener('click', () => {
+    if (loginBox.style.display === 'block') {
+        loginBox.style.display = 'none';
+    } else {
+        loginBox.style.display = 'block';
+    }
+});
 
 // Event listeners for close icons
 closeIcons.forEach(icon => {
