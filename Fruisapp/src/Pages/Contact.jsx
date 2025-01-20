921a3aee9499e5a219dfec2ca8edeb906403ffea
Ravi Shankar Singh <civilstudy2024@gmail.com>
2025-01-20 22:20:19 +0530

    Update

--- Fruisapp/src/Pages/Contact.jsx
+++ Fruisapp/src/Pages/Contact.jsx
@@ -36,15 +36,15 @@ const Contact = () => {
             </div>
             <div className="col-lg-12">
               <div className="h-100 rounded">
-                <iframe
-                title="mfkdfmkd"
-                  className="rounded w-100"
-                  style={{ height: 400 }}
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.33750346623!2d-73.97968099999999!3d40.6974881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1694259649153!5m2!1sen!2sbd"
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
-                />
-              </div>
+              <iframe
+                    title="GK2 Map"
+                    className="rounded w-100"
+                    style={{ height: 400 }}
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224344.56317691595!2d77.20390307821788!3d28.53551693506127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce3e186122455%3A0xe98016d5f05e482a!2sGreater%20Kailash%2C%20New%20Delhi%2C%20Delhi%20110048%2C%20India!5e0!3m2!1sen!2sin!4v1694259649153!5m2!1sen!2sin"
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                  />
+             </div>
             </div>
             <div className="col-lg-7">
               <form action="" className="">
@@ -78,21 +78,21 @@ const Contact = () => {
                 <i className="fas fa-map-marker-alt fa-2x text-primary me-4" />
                 <div>
                   <h4>Address</h4>
-                  <p className="mb-2">123 Street New York.USA</p>
+                  <p className="mb-2"> 504, Space Time Gk-2 New Delhi 110020</p>
                 </div>
               </div>
               <div className="d-flex p-4 rounded mb-4 bg-white">
                 <i className="fas fa-envelope fa-2x text-primary me-4" />
                 <div>
                   <h4>Mail Us</h4>
-                  <p className="mb-2">info@example.com</p>
+                  <p className="mb-2">informfurites@gmail.com</p>
                 </div>
               </div>
               <div className="d-flex p-4 rounded bg-white">
                 <i className="fa fa-phone-alt fa-2x text-primary me-4" />
                 <div>
                   <h4>Telephone</h4>
-                  <p className="mb-2">(+012) 3456 7890</p>
+                  <p className="mb-2">7982464913</p>
                 </div>
               </div>
             </div>
